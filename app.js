ce812887a3771b22a62a73ea31fecf4b7a88cc7b
Bibizareen <142078516+Bibizareen@users.noreply.github.com>
2023-10-06 22:17:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+//  Practise exercise 1.1
+// Question 1
+// Working on Console is , the response is 14
+    // Question 2
+    console.log("Bibi Zareen")
+    //  Practise Exercise 1.2
+    // Working on Html File
+    //  Practise Exercise 1.3
+    console.log("Bibi Zareen");
+    alert("Hello World");
+    //  Practise Exercise 1.4
+    let b = 20  //We can use the variable code to store 20 value
+    console.log( b ) //console.log show the output of the variable code 
+    // Single line comment is the inline comment at the end of line it is started to written with // on the line which get ignored
+    // Multiple line comment started with /*  and ending with */
+    // Chapter Project
+    // console.log("bibi zareen")
+    //  Self Quiz
+    // 1-    <script src="app.js"></script>
+    // 2-     yes 
+    // 3-     We can use Mutiple line comment by using /* and end with */
+    // 4-      We can simply add comment.
